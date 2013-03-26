5733c274ffe60252310cd7d9738b241689ceb13f
Lee Hambley <lee.hambley@gmail.com>
2013-03-26 12:30:24 +0100

    Weiter

--- Gemfile
+++ Gemfile
@@ -1,10 +1,12 @@
 # A sample Gemfile
 source "https://rubygems.org"
 
+gem 'builder'
 gem 'haml'
 gem 'i18n'
 gem 'puma'
 gem 'redcarpet'
+gem 'ruby-libvirt', require: 'libvirt'
 gem 'sinatra'
 gem 'sinatra-contrib'
 gem 'sinatra-i18n'
--- app/mvmc.rb
+++ app/mvmc.rb
@@ -1,129 +1,262 @@
-require 'open3'
-require 'ostruct'
+require 'rexml/document'
+require 'builder'
 require 'sinatra/base'
-require 'sinatra/i18n'
 require 'sinatra/content_for'
+require 'sinatra/i18n'
 
-VMS_DIR  = File.expand_path(File.join(File.dirname(__FILE__), '../vms/'))
-ISOS_DIR = File.expand_path(File.join(File.dirname(__FILE__), '../isos/'))
+VIRSH_HOST = "protonet@cebit.local"
+VIRSH_URI  = "qemu+ssh://#{VIRSH_HOST}/system?socket=/var/run/libvirt/libvirt-sock"
+
+VMS_DIR    = File.expand_path(File.join(File.dirname(__FILE__), '../vms/'))
+ISOS_DIR   = File.expand_path(File.join(File.dirname(__FILE__), '../isos/'))
 
 ISO = Struct.new(:basename, :mtime, :size)
 
-class Runner
+module Virsh
 
-  Result = Struct.new(:output, :status) do
-    def success?
-      status.success?
+  class StoragePool
+
+    attr_accessor :name, :uuid
+
+    def num_of_volumes
+      pool.num_of_volumes
     end
-    def lines
-      output.lines.to_a.map(&:strip).reject(&:empty?)
+
+    def volumes
+      pool.list_volumes.collect do |volume_name|
+        pool.lookup_volume_by_name(volume_name)
+      end
     end
-  end
 
-  def self.wrap_with
-    if ENV["RACK_ENV"].to_sym == :development
-      'ssh -o "ProxyCommand nc -X connect -x ssh.protonet.info:8022 %%h %%p" -o "User protonet" cebit %s'
-    else
-      'env -i %s'
+    def create_volume(name, capacity)
+      pool.create_volume_xml(create_volume_xml_string(name, capacity))
     end
+
+    class << self
+      def find(uuid)
+        pool = libvirt.lookup_storage_pool_by_uuid(uuid)
+        StoragePool.new.tap do |sp|
+          sp.uuid = uuid
+        end
+      end
+      def create_default
+        libvirt.define_storage_pool_xml(create_pool_xml).tap do |pool|
+          pool.build
+          pool.create
+          pool.autostart = true
+        end
+      end
+      def all
+        libvirt.list_storage_pools.collect do |pool_name|
+          pool = libvirt.lookup_storage_pool_by_name(pool_name)
+          StoragePool.new.tap do |sp|
+            sp.name = pool.name
+            sp.uuid = pool.uuid
+          end
+        end
+      end
+
+      def libvirt
+        @_libvirt ||= Libvirt::open(VIRSH_URI)
+      end
+
+      private
+
+      def create_pool_xml
+        xml = Builder::XmlMarkup.new
+        xml.instruct! :xml, :version => '1.1'
+        xml.pool type: :dir do |pool|
+          pool.name "virsh-images"
+          pool.target do |target|
+            target.path "/root/virsh-images"
+          end
+        end
+        warn xml.target!
+        xml.target!
+      end
+    end
+
+    def create_volume_xml_string(name, capacity)
+      xml = Builder::XmlMarkup.new
+      xml.instruct! :xml, :version => '1.1'
+      xml.volume do |volume|
+        volume.name "#{name}.img"
+        volume.capacity capacity, unit: :G
+      end
+    end
+
+    def pool
+      libvirt.lookup_storage_pool_by_uuid(uuid)
+    end
+
+    def libvirt
+      self.class.libvirt
+    end
+
   end
 
-  def self.command(c)
-    c    = wrap_with % c
-    o, s = Open3.capture2e(c)
-    Result.new(o, s)
-  end
-
-end
-
-class VMHost
-  def self.private_ip
-    r = Runner.command "ifconfig eth0 | grep 'inet addr:' | cut -f2 -d: | awk '{ print $1 }'"
-    r.lines.join('').strip
-  end
 end
 
 class VM
 
-  attr_reader :number, :name, :state
-
-  def initialize(number, name, state)
-    @number, @name, @state = number, name, state
-  end
-
-  def vnc_port
-    5900 + number.to_i
-  end
+  attr_accessor :id, :uuid, :name
 
   def running?
-    state =~ /running/
+    domain.active?
   end
 
-  def self.all
-    if r = Runner.command('virsh list --inactive --all')
-      if r.success?
-        r.lines[2..-1].collect do |line|
-          res = line.split("\s", 3).map(&:strip)
-          new(*res)
+  def start
+    domain.create
+  end
+
+  def stop
+    domain.stop
+  end
+
+  def shutdown
+    domain.shutdown
+  end
+
+  def state
+    domain.state
+  end
+
+  def vnc_address_with_port
+    doc      = REXML::Document.new(domain.xml_desc)
+    elements = doc.elements.to_a('/domain/devices/graphics')
+    ports    = elements.collect { |e| e.attribute(:port).value }
+    port     = ports.first
+    "vnc://#{VIRSH_HOST}:#{port}"
+  end
+
+  class << self
+
+    def all
+      defined + running
+    end
+
+    def defined
+     libvirt.list_defined_domains.collect do |domain_name|
+        domain = libvirt.lookup_domain_by_name(domain_name)
+        VM.new.tap do |vm|
+          vm.id   = domain.id rescue nil
+          vm.uuid = domain.uuid
+          vm.name = domain_name
         end
       end
-    else
-      []
     end
-  end
 
-  def self.create(name, bootiso, cdiso)
-    hdd_r = create_hdd(name)
-    if hdd_r.success?
-      create(name, bootiso, cdiso)
-    else
-      raise hdd_r.output
+    def running
+      libvirt.list_domains.collect do |domain_id|
+        domain = libvirt.lookup_domain_by_id(domain_id)
+        VM.new.tap do |vm|
+          vm.id   = domain_id
+          vm.uuid = domain.uuid
+          vm.name = domain.name
+        end
+      end
     end
-  end
 
-  def self.create_hdd(name)
-    Runner.command(create_hdd_command(name))
-  end
-
-  def self.create_hdd_command(name)
-    "sudo qemu-img create \"#{name}\" \"#{opts.vm_hdd_image_size}\" \"#{opts.vm_hdd_image % name}\""
-  end
-
-  def self.create(name, bootiso, cdiso)
-    Runner.command(create_command(name, bootiso, cdiso))
-  end
-
-  def self.create_command(name, bootiso, cdiso = '')
-    cc = <<-EOCREATECOMMAND.unindent.gsub(/\s+/, ' ')
-      sudo virt-install                                                   \
-        --force                                                           \
-        --graphics vnc,listen=0.0.0.0 --noautoconsole                     \
-        --arch=x86_64                                                     \
-        --connect "#{opts.libvirt_domain}"                                \
-        --disk="#{opts.vm_hdd_image % name}",bus=virtio,cache=none        \
-        --network bridge:virbr0,model=e1000                               \
-        --vcpus="#{opts.vm_num_cpus}"                                     \
-        -c "#{File.join(VMS_DIR, bootiso)}"                              \
-        -n "#{name}"                                                      \
-        -r "#{opts.vm_ram}"
-    EOCREATECOMMAND
-    if cdiso
-      cc.lines.insert(6, "--disk path=\"#{File.join(VMS_DIR, cdiso)}\",device=cdrom,perms=ro")
+    def libvirt
+      @_libvirt ||= Libvirt::open(VIRSH_URI)
     end
+
+    def create(name, cdiso_paths, volume_paths)
+      domain = libvirt.define_domain_xml(create_xml(name, cdiso_paths, volume_paths))
+      domain.tap do |d|
+        domain.autostart = true
+        domain.create
+      end
+    end
+
+    def create_xml(name, cdisos, volume_paths)
+
+      xml = Builder::XmlMarkup.new
+      xml.instruct! :xml, :version => '1.1'
+
+      bus  = -1
+      devs = ('hda'...'hde').to_a
+      vdas = ('vda'...'vde').to_a
+
+      xml.domain type: :kvm do |domain|
+
+        # Hardware clock in local time
+        domain.clock sync: :localtime
+
+        domain.name name
+
+        domain.memory         262144
+        domain.current_memory 262144
+
+        domain.vcpu 2
+
+        domain.os do |os|
+          os.type "hvm", arch: :i686, machine: :pc
+          os.boot dev: :cdrom
+        end
+
+        domain.devices do |devices|
+
+          devices.emulator "/usr/bin/kvm"
+
+          cdisos.each do |cdiso|
+            devices.disk type: :file, device: :cdrom do |disk|
+              disk.driver name: :qemu, type: :raw
+              disk.source file: cdiso
+              disk.target dev: devs.shift, bus: :ide
+              disk.readonly
+              disk.address type: :drive, controller: 0, bus: (bus += 1), unit: 0
+            end
+          end
+
+          volume_paths.each do |volume_path|
+            devices.disk do |disk|
+              disk.driver name: :qemu, type: :raw, cache: :none
+              disk.source file: volume_path
+              disk.target dev: vdas.shift, bus: :virtio
+              disk.address type:      :pci,
+                           domain:    '0x0000',
+                           bus:       '0x0000',
+                           slot:      '0x04',
+                           function:  '0x00'
+            end
+          end
+
+          devices.controller type: :ide, index: 0 do |controller|
+            controller.address type:      :pci,
+                               domain:    '0x0000',
+                               bus:       '0x0000',
+                               slot:      '0x01',
+                               function:  '0x01'
+          end
+
+          devices.interface type: :network do |interface|
+            interface.source network: :default
+          end
+
+          devices.video do |video|
+            video.model   type: :vga, vram: 262144, heads: 1
+            video.address type: :pci, domain: '0x0000', bus: '0x00', slot: '0x02', function: '0x0'
+          end
+
+          devices.graphics type: :vnc, port: -1
+
+        end
+
+      end
+      xml.target!
+    end
+
   end
 
-  def self.opts
-    OpenStruct.new(options_hash)
+  private
+
+  def libvirt
+    self.class.libvirt
   end
 
-  def self.options_hash
-    {
-      :vm_hdd_image      => '~/vms/%s/hdd.img',
-      :vm_hdd_image_size => '50G',
-      :vm_num_cpus       => '2',
-      :vm_ram            => 4069,
-      :libvirt_domain    => 'qemu:///system'
-    }
+  def domain
+    libvirt.lookup_domain_by_uuid(uuid)
   end
 
 end
@@ -135,6 +268,10 @@ class Mvmc < Sinatra::Base
 
   helpers Sinatra::ContentFor
   register Sinatra::I18n
+
+  before do
+    Virsh::StoragePool.create_default if Virsh::StoragePool.all.empty?
+  end
 
   get '/' do
     redirect to('/dashboard'), 303
@@ -156,19 +293,37 @@ class Mvmc < Sinatra::Base
   end
 
   post '/vms' do
-    @create_hdd_command = VM.create_hdd_command(params["name"])
-    @create_command      = VM.create_command(params["name"], params["bootiso"], params["cdiso"])
-    return [@create_hdd_command, @create_command].inspect
+    pool = Virsh::StoragePool.all.first
+    hdd_images = params[:vm][:volumes].collect do |i, volume_params|
+      pool.create_volume(volume_params[:name], volume_params[:capacity]).path
+    end
+    VM.create(params[:vm][:name], params[:vm][:cdisos].values, hdd_images)
+    redirect '/vms'
   end
 
-  post '/vms/:name/start' do |name|
-    VM.new(nil, name, nil).start
-    status 204
+  get '/vms/:uuid/start' do |uuid|
+    VM.new.tap do |vm|
+      vm.uuid = uuid
+    end.start
+    redirect '/vms'
   end
 
-  post '/vms/:name/stop' do |name|
-    VM.new(nil, name, nil).stop
-    status 204
+  get '/storage/volume.xml' do
+    builder :'storage/volume'
+  end
+
+  get '/vms/:uuid/shutdown' do |uuid|
+    VM.new.tap do |vm|
+      vm.uuid = uuid
+    end.shutdown
+    redirect '/vms'
+  end
+
+  get '/vms/:uuid/stop' do |uuid|
+    VM.new.tap do |vm|
+      vm.uuid = uuid
+    end.stop
+    redirect '/vms'
   end
 
   get '/isos/:basename' do |basename|
@@ -182,6 +337,11 @@ class Mvmc < Sinatra::Base
       end
     end
     status 200
+  end
+
+  get '/pools' do
+    @pools = Virsh::StoragePool.all
+    haml :'pools/index'
   end
 
   private
